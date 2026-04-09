f2b7b07810ff658b0168776d6b91a3cb5f69f3bf
niltinfilho <niltonfilho115@gmail.com>
2026-04-09 13:53:31 -0300

    feat: create types and default path;

---
+++ src/types.ts
@@ -0,0 +1,36 @@
+export interface ItemMenu {
+  uuidItemMenu: string;
+  base64Image: string;
+  descricaoAltImage: string;
+  nome: string;
+  descricao: string;
+  categoria: string;
+  descricaoCategoria: string;
+};
+
+export interface Categoria {
+  valor: string;
+  descricao: string;
+};
+
+export interface Item {
+  uuidItemMenu: string;
+  base64Image: string;
+  descricaoAltImage: string;
+  nome: string;
+  descricao: string;
+};
+
+export interface ItemMenuDetalhes {
+  categoria: string;
+  descricaoCategoria: string;
+  itens: Item[];
+};
+
+export interface FormDataCadastrar {
+  nome: string;
+  categoria: string;
+  descricao: string;
+  base64Image: string;
+  descricaoAltImage: string;
+};
