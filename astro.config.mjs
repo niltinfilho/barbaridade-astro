851fab56cac534cf41abd52748bb80f82d77c919
niltinfilho <niltonfilho115@gmail.com>
2026-04-22 20:04:53 -0300

    feat: instalando dependencias do tailwindcss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://niltinfilho.github.io',
-  base: '/barbaridade-astro',
+  site: "https://niltinfilho.github.io",
+  base: "/barbaridade-astro",
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
