49c17152cc8f1312d636bbfb13d27c09382d699e
niltinfilho <niltonfilho115@gmail.com>
2026-04-25 15:16:33 -0300

    feat: criando novo css para os cards de item-menu; removendo a estilização da página cardapio

--- src/scripts/api/buildMenuItens.ts
+++ src/scripts/api/buildMenuItens.ts
@@ -1,15 +1,16 @@
 import type { ItemMenuDetalhes } from "@types";
 
 export async function renderItems(itemMenuDetalhesData: ItemMenuDetalhes[]) {
-  const container = document.querySelector(".menu-grid.container");
+  const container = document.querySelector("#menu-itens-root");
   container!.innerHTML = "";
 
   itemMenuDetalhesData.forEach((itemMenuDetalhes: ItemMenuDetalhes) => {
     if (itemMenuDetalhes.itens.length > 0) {
       const section = document.createElement("div");
-      section.classList.add("section");
+      section.classList.add("menu-itens-section");
 
       const h2 = document.createElement("h2");
+      h2.classList.add("title-category-menu-itens");
       h2.textContent = itemMenuDetalhes.descricaoCategoria;
       section.appendChild(h2);
 
@@ -21,15 +22,18 @@ export async function renderItems(itemMenuDetalhesData: ItemMenuDetalhes[]) {
         article.classList.add("card");
 
         const img = document.createElement("img");
+        img.classList.add("image-card-item-menu")
         img.src = item.base64Image;
         img.alt = item.descricaoAltImage;
         article.appendChild(img);
 
         const h3 = document.createElement("h3");
+        h3.classList.add("title-card-item-menu")
         h3.textContent = item.nome;
         article.appendChild(h3);
 
         const p = document.createElement("p");
+        p.classList.add("text-card-item-menu")
         p.textContent = item.descricao;
         article.appendChild(p);
         cardsDiv.appendChild(article);
